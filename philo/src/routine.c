a3208479c1cd2d045aa50c84429a5b33fd82fab0
MarJC5 <jmartin@student.42lausanne.ch>
2022-04-04 16:39:00 +0200

    Fix fork lock when many philo

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 12:50:30 by jmartin           #+#    #+#             */
-/*   Updated: 2022/04/04 14:57:48 by jmartin          ###   ########.fr       */
+/*   Updated: 2022/04/04 16:36:38 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,7 @@ int	routine_fork(t_philo *philo)
 
 void	routine_sleep(t_philo *philo)
 {
-	if (philo->is_dead != 1
-		&& philo->status->state < 1)
+	if (philo->status->state < 1)
 	{
 		print_event(philo, "is \033[1;36msleeping\033[0m",
 			philo->position, now(philo));
@@ -57,10 +56,10 @@ void	routine_eat(t_philo *philo)
 		print_event(philo, "is \033[1;32meating\033[0m", philo->position,
 			philo->last_meal);
 		oh_wait(philo, philo->status->time_to_eat);
-		pthread_mutex_unlock(&philo->fork);
-		pthread_mutex_unlock(&philo->l_fork->fork);
 		philo->eat_count += 1;
 		if (philo->eat_count == philo->status->num_of_times_to_eat)
 			philo->status->all_eaten += 1;
 	}
+	pthread_mutex_unlock(&philo->fork);
+	pthread_mutex_unlock(&philo->l_fork->fork);
 }
